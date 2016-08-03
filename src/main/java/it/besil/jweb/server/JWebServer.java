20401c7e44af6a75728e6ffe9a4ed45dac8a93c6
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2016-08-03 12:17:36 +0200

    Refact

--- src/main/java/it/besil/jweb/server/JWebServer.java
+++ src/main/java/it/besil/jweb/server/JWebServer.java
@@ -7,6 +7,7 @@ import it.besil.jweb.app.resources.JWebResource;
 import it.besil.jweb.server.conf.JWebConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import spark.Route;
 import spark.Service;
 
@@ -20,11 +21,14 @@ import java.util.List;
  */
 public class JWebServer {
     private final Service http;
-    private Logger log = LoggerFactory.getLogger(JWebServer.class);
+    private Logger log;
     private List<JWebApp> apps;
 
     public JWebServer(JWebConfiguration conf) {
-        log.info("Using conf\n{}", conf.toString());
+        if (conf.debugMode())
+            System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, Level.DEBUG.name());
+        log = LoggerFactory.getLogger(JWebServer.class);
+        log.debug("Using conf\n{}", conf.toString());
         this.http = Service.ignite();
         this.apps = new LinkedList<>();
     }
@@ -37,6 +41,7 @@ public class JWebServer {
                 this.install(resource);
             }
         }
+        log.info("Server started");
     }
 
     private void install(JWebResource resource) {
