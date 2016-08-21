25e9d2130a2e2ace428b6b78a18a61a5c9a3a99d
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2016-08-21 15:51:15 +0200

    Updated documentation

--- src/main/java/it/besil/jweb/app/answer/Answer.java
+++ src/main/java/it/besil/jweb/app/answer/Answer.java
@@ -5,25 +5,4 @@ package it.besil.jweb.app.answer;
  */
 public interface Answer {
 
-//    public static final int SUCCESS = 200;
-//    private final int status;
-//
-//    public Answer(int status) {
-//        this.status = status;
-//    }
-//
-//    public int getStatus() {
-//        return status;
-//    }
-//
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (obj instanceof Answer)
-//            return this.equals((Answer) obj);
-//        return super.equals(obj);
-//    }
-//
-//    public boolean equals(Answer a) {
-//        return this.getStatus() == a.getStatus();
-//    }
 }
