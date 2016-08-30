5a94966b626bf8b1f5493d8f88f24a88d9d1115f
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2016-08-30 10:19:55 +0200

    closes #1

--- src/main/java/it/besil/jweb/app/payloads/Payload.java
+++ src/main/java/it/besil/jweb/app/payloads/Payload.java
@@ -3,11 +3,23 @@ package it.besil.jweb.app.payloads;
 import spark.Request;
 import spark.Response;
 
+import java.lang.reflect.Field;
+
 /**
  * Created by besil on 01/05/2016.
  */
 public interface Payload {
 
-    void init(Request req, Response resp);
+    default void init(Request req, Response resp) {
+        Field[] fields = this.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                field.set(this, req.queryParams(field.getName()));
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
