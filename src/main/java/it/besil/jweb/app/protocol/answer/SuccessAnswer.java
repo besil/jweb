f575567a7f30fbc4e78f6174a50f1722ffcbe3ee
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2016-09-02 11:04:56 +0200

    Bug fixing

--- src/main/java/it/besil/jweb/app/protocol/answer/SuccessAnswer.java
+++ src/main/java/it/besil/jweb/app/protocol/answer/SuccessAnswer.java
@@ -4,15 +4,11 @@ package it.besil.jweb.app.protocol.answer;
  * Created by besil on 03/08/2016.
  */
 public class SuccessAnswer extends ReturnCodeMessageAnswer {
-    private final String message;
-
     public SuccessAnswer(int status, String message) {
         super(status, message);
-        this.message = message;
     }
 
     public SuccessAnswer(String message) {
         this(200, message);
     }
-
 }
